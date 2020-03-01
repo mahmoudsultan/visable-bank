0db3e2470e9ce4124e9c9e8177c530207da3f7be
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2020-03-01 14:14:51 +0200

    Fetch transactions for a BankAccount using the two forign keys

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -2,4 +2,8 @@
 
 class BankAccount < ApplicationRecord
   validates :account_number, uniqueness: true
+
+  def transactions
+    Transaction.where('to_account_id = ? OR from_account_id = ?', id, id)
+  end
 end
--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -3,5 +3,64 @@
 require 'rails_helper'
 
 RSpec.describe BankAccount, type: :model do
-  it { is_expected.to validate_uniqueness_of :account_number }
+  describe 'Validations' do
+    it { is_expected.to validate_uniqueness_of :account_number }
+  end
+
+  describe 'Transactions' do
+    context 'when there\'s transaction from this account' do
+      let!(:bank_account) { FactoryBot.create :bank_account }
+
+      before do
+        another_bank_account = FactoryBot.create :bank_account
+        FactoryBot.create(:transaction,
+                          from_account: bank_account,
+                          to_account: another_bank_account)
+      end
+
+      it 'returns one transaction' do
+        expect(bank_account.transactions.length).to be 1
+      end
+    end
+
+    context 'when there\'s transaction to this account' do
+      let!(:bank_account) { FactoryBot.create :bank_account }
+
+      before do
+        another_bank_account = FactoryBot.create :bank_account
+        FactoryBot.create(:transaction,
+                          from_account: another_bank_account,
+                          to_account: bank_account)
+      end
+
+      it 'returns one transaction' do
+        expect(bank_account.transactions.length).to be 1
+      end
+    end
+
+    context 'when there are one transaction from this account and another to it' do
+      let!(:first_bank_account) { FactoryBot.create :bank_account }
+      let!(:second_bank_account) { FactoryBot.create :bank_account }
+
+      let!(:first_transaction) do
+        FactoryBot.create(:transaction,
+                          from_account: first_bank_account,
+                          to_account: second_bank_account)
+      end
+      let!(:second_transaction) do
+        FactoryBot.create(:transaction,
+                          from_account: second_bank_account,
+                          to_account: first_bank_account)
+      end
+
+      it 'returns 2 transactions' do
+        expect(first_bank_account.transactions.length).to be 2
+      end
+
+      it 'returns transactions sorted by createdAt' do
+        expect([first_bank_account.transactions.first,
+                first_bank_account.transactions.second]).to eql [first_transaction, second_transaction]
+      end
+    end
+  end
 end
