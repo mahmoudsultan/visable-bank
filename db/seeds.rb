21d0c60550859825b61b6c65eb524595b1e91cd3
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2020-03-01 14:26:52 +0200

    Seed file for BankAccount and Transactions.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+FIRST_BANK_ACCOUNT_NUMBER = '100-001-010'
+SECOND_BANK_ACCOUNT_NUMBER = '101-010-011'
+TRANSACTIONS_COUNT_TO_ACCOUNT = 10
+
+first_bank_account = BankAccount.create_with(balance: 100)
+                                .find_or_create_by(account_number: FIRST_BANK_ACCOUNT_NUMBER)
+
+
+second_bank_account = BankAccount.create_with(balance: 100)
+                                 .find_or_create_by(account_number: SECOND_BANK_ACCOUNT_NUMBER)
+
+first_bank_account.transactions.delete_all
+second_bank_account.transactions.delete_all
+
+TRANSACTIONS_COUNT_TO_ACCOUNT.times do
+  Transaction.create(to_account: first_bank_account, from_account: second_bank_account, amount: rand(1...1000))
+  Transaction.create(to_account: second_bank_account, from_account: first_bank_account, amount: rand(1...1000))
+end
